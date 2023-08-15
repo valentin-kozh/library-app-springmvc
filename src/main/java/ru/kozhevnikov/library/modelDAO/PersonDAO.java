388f160d5b4d1956bbfa49271667a5450dec00f7
kozhevnikov valentin <Kozhevnikov.VD@yandex.ru>
2023-08-15 15:19:29 +0300

    add R for Person

---
+++ src/main/java/ru/kozhevnikov/library/modelDAO/PersonDAO.java
@@ -0,0 +1,29 @@
+package ru.kozhevnikov.library.modelDAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.kozhevnikov.library.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE person_id=?", new Object[]{id},
+                new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
+    }
+
+}
