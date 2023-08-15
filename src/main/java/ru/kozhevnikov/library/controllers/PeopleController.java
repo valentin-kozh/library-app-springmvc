b5facb86d90932489a660ff3d7cdcaa894384c96
kozhevnikov valentin <Kozhevnikov.VD@yandex.ru>
2023-08-15 17:22:36 +0300

    add U to Person

--- src/main/java/ru/kozhevnikov/library/controllers/PeopleController.java
+++ src/main/java/ru/kozhevnikov/library/controllers/PeopleController.java
@@ -36,4 +36,14 @@ public class PeopleController {
         personDAO.save(person);
         return "redirect:/people";
     }
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model){
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id") int id, @ModelAttribute("person") Person person){
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
 }
