c863380515afbe44bf71cba33df54dcf78fa114c
kozhevnikov valentin <Kozhevnikov.VD@yandex.ru>
2023-08-15 15:39:31 +0300

    add C for Person

--- src/main/java/ru/kozhevnikov/library/modelDAO/PersonDAO.java
+++ src/main/java/ru/kozhevnikov/library/modelDAO/PersonDAO.java
@@ -20,10 +20,13 @@ public class PersonDAO {
     public List<Person> index() {
         return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
-
     public Person show(int id) {
         return jdbcTemplate.query("SELECT * FROM Person WHERE person_id=?", new Object[]{id},
                 new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
     }
-
+    public void save(Person person){
+        jdbcTemplate.update(
+          "INSERT INTO Person (name, year) VALUES (?,?)", person.getName(), person.getYear()
+        );
+    }
 }
