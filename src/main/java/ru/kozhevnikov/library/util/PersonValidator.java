7433ea28590f1a639b55090b0d43a173f8dc30b5
kozhevnikov valentin <Kozhevnikov.VD@yandex.ru>
2023-08-15 18:51:59 +0300

    add validation to Person

---
+++ src/main/java/ru/kozhevnikov/library/util/PersonValidator.java
@@ -0,0 +1,31 @@
+package ru.kozhevnikov.library.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.kozhevnikov.library.model.Person;
+import ru.kozhevnikov.library.modelDAO.PersonDAO;
+
+@Component
+public class PersonValidator implements Validator {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+        if (personDAO.show(person.getName()).isPresent())
+            errors.rejectValue("name", "", "Пользователь с таким именем уже существует");
+        if (person.getYear() <= 1900) errors.rejectValue("year", "", "Год указан неверно");
+    }
+}
