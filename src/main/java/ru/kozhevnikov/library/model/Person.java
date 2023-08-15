202a36076be7970cf3b2423b6e46069a86e10136
kozhevnikov valentin <Kozhevnikov.VD@yandex.ru>
2023-08-15 18:51:59 +0300

    add validation to Person

--- src/main/java/ru/kozhevnikov/library/model/Person.java
+++ src/main/java/ru/kozhevnikov/library/model/Person.java
@@ -1,8 +1,15 @@
 package ru.kozhevnikov.library.model;
 
+import javax.validation.constraints.*;
+
 public class Person {
     private int personId;
+    @NotBlank(message = "Поле ФИО не может быть пустым")
+    @Size(min = 2, max = 100, message = "Количество символов в ФИО должно бытть между 2 и 100")
+    @Pattern(regexp = "^[^0-9]*$", message = "В ФИО не могут использоваться цифры")
+    @Pattern(regexp = "^[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+$", message = "ФИО должно быть указано в формате: Иванов Иван Иванович")
     private String name;
+    @Max(value = 2022, message = "Год рождения введен некорректно")
     private int year;
 
     public Person() {
